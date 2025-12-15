795dc2d030e5ca7c68f05eea2fca64f68b9de529
Random2127 <vmedina1978@gmail.com>
2025-12-15 12:43:31 +0100

    Main screens and default bars

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fogonesia/screens/chat_screen.dart';
+import 'package:fogonesia/screens/dietary_screen.dart';
+import 'package:fogonesia/screens/recipes_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +11,37 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  late final List<Widget> _screens = [
+    const ChatScreen(),
+    const RecipesScreen(),
+    const DietaryScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Home Page'));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FogonesIA'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      ),
+      body: IndexedStack(index: _selectedIndex, children: _screens),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
+          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Receta'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Parámetros',
+          ),
+        ],
+      ),
+    );
   }
 }
