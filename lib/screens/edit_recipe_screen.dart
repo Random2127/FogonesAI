45283a261735c1bea007aaff3f7c9d6fcb8e2eed
Random2127 <vmedina1978@gmail.com>
2025-12-16 13:39:54 +0100

    routes file

--- lib/screens/edit_recipe_screen.dart
+++ lib/screens/edit_recipe_screen.dart
@@ -1,1 +1,11 @@
 // edit saved recipe
+import 'package:flutter/material.dart';
+
+class EditRecipeScreen extends StatelessWidget {
+  const EditRecipeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:fogonesia/screens/chat_screen.dart';
+import 'package:fogonesia/screens/dietary_screen.dart';
+import 'package:fogonesia/screens/edit_recipe_screen.dart';
+import 'package:fogonesia/screens/home_page.dart';
+import 'package:fogonesia/screens/login.dart';
+import 'package:fogonesia/screens/recipes_screen.dart';
+import 'package:fogonesia/screens/register.dart';
+
+class Routes {
+  static const String home = '/home';
+  static const String chat = '/chat';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String recipes = '/recipes';
+  static const String dietary = '/dietary';
+  static const String editRecipe = '/editRecipe';
+
+  static Map<String, WidgetBuilder> routes = {
+    home: (context) => const HomePage(),
+    chat: (context) => const ChatScreen(),
+    login: (context) => const Login(),
+    register: (context) => const Register(),
+    recipes: (context) => const RecipesScreen(),
+    dietary: (context) => const DietaryScreen(),
+    editRecipe: (context) => const EditRecipeScreen(),
+  };
+}
