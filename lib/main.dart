0a767e9c6d71cf3277dd838e2795656f878d8e4e
Sergio <sergio1997lorenzo@gmail.com>
2026-02-07 14:12:54 +0100

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:fogonesia/controllers/theme_controller.dart';
 import 'package:fogonesia/models/recipe.dart';
 import 'package:fogonesia/screens/edit_recipe_screen.dart';
 import 'package:fogonesia/screens/recipe_details_screen.dart';
+import 'package:fogonesia/screens/splash_screen.dart'; 
+import 'package:fogonesia/screens/home_page.dart';
 import 'package:fogonesia/theme/app_theme.dart' as app_theme;
 import 'package:fogonesia/utils/routes.dart';
 import 'package:provider/provider.dart';
@@ -12,7 +14,6 @@ import 'package:fogonesia/app/app_bootstrap.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // API key
   await dotenv.load(fileName: ".env");
   final sharedPrefs = await SharedPreferences.getInstance();
 
@@ -25,32 +26,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = context.watch<ThemeController>();
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: app_theme.lightMode,
       darkTheme: app_theme.darkMode,
       themeMode: theme.themeMode,
-      initialRoute: Routes.home,
-      routes: Routes.routes,
+      
+      initialRoute: Routes.splash, 
 
-      // This may not be the cleanest but it gets the job done
-      // Only way I could figure out to pass arguments to routes
       onGenerateRoute: (settings) {
         switch (settings.name) {
+          case Routes.splash:
+            return MaterialPageRoute(builder: (_) => const SplashScreen());
+          
+          case Routes.home:
+            return MaterialPageRoute(builder: (_) => const HomePage());
+
           case Routes.recipeDetails:
-            final title = settings.arguments as String;
-            return MaterialPageRoute(
-              builder: (_) => RecipeDetailsScreen(recipeTitle: title),
-            );
+            if (settings.arguments is String) {
+              final title = settings.arguments as String;
+              return MaterialPageRoute(
+                builder: (_) => RecipeDetailsScreen(recipeTitle: title),
+              );
+            }
+            return null;
 
           case Routes.editRecipe:
-            final recipe = settings.arguments as Recipe;
-            return MaterialPageRoute(
-              builder: (_) => EditRecipeScreen(recipe: recipe),
-            );
+            if (settings.arguments is Recipe) {
+              final recipe = settings.arguments as Recipe;
+              return MaterialPageRoute(
+                builder: (_) => EditRecipeScreen(recipe: recipe),
+              );
+            }
+            return null;
+            
+          default:
+            // Si la ruta no existe, mandamos al Home para evitar la "X"
+            return MaterialPageRoute(builder: (_) => const HomePage());
         }
-        return null;
       },
     );
+  
   }
 }
